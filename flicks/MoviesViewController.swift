07df552df66b86f735f0f07e7b50d43b3b079708
Kristy Caster <kristyacaster@gmail.com>
2016-11-04 17:08:42 -0700

    support infinite scrolling

--- flicks/MoviesViewController.swift
+++ flicks/MoviesViewController.swift
@@ -1,32 +1,36 @@
-//
-//  MoviesViewController.swift
-//  flicks
-//
-//  Created by Kristy Caster on 11/4/16.
-//  Copyright © 2016 kristeaac. All rights reserved.
-//
-
 import UIKit
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     var movies = [NSDictionary]()
-    var page: Int!
+    var page = 1
     var totalPages: Int!
     var totalResults: Int!
+    var isMoreDataLoading = false
+    var loadingMoreView: InfiniteScrollActivityView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        TheMovieDBHelper.getNowPlaying(callback: handleMovieList)
+        TheMovieDBHelper.getNowPlaying(page: page, callback: handleMovieList)
+        initializeInfiniteScroll()
     }
     
     func handleMovieList(movies: [NSDictionary]?, page: Int, totalPages: Int, totalResults: Int) {
-        self.movies = movies!
+        self.isMoreDataLoading = false
+        self.loadingMoreView!.stopAnimating()
+        self.movies.append(contentsOf: movies!)
         tableView.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func initializeInfiniteScroll() {
+        let frame = CGRect(origin: CGPoint(x: 0,y :tableView.contentSize.height), size: CGSize(width: tableView.bounds.size.width, height: InfiniteScrollActivityView.defaultHeight))
+        loadingMoreView = InfiniteScrollActivityView(frame: frame)
+        loadingMoreView!.isHidden = true
+        tableView.addSubview(loadingMoreView!)
+        
+        var insets = tableView.contentInset;
+        insets.bottom += InfiniteScrollActivityView.defaultHeight;
+        tableView.contentInset = insets
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -34,6 +38,27 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if (indexPath.row == movies.count - 1) {
+            loadMoreMovies()
+        }
+        return populateCellWithMoveDetails(indexPath: indexPath)
+    }
+    
+    func loadMoreMovies() {
+        if (!isMoreDataLoading) {
+            isMoreDataLoading = true
+            animateLoadingIndicator()
+            TheMovieDBHelper.getNowPlaying(page: page + 1, callback: handleMovieList)
+        }
+    }
+    
+    func animateLoadingIndicator() {
+        let frame = CGRect(origin: CGPoint(x: 0,y :tableView.contentSize.height), size: CGSize(width: tableView.bounds.size.width, height: InfiniteScrollActivityView.defaultHeight))
+        loadingMoreView?.frame = frame
+        loadingMoreView!.startAnimating()
+    }
+    
+    func populateCellWithMoveDetails(indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         let movie = movies[indexPath.row]
         let title = movie.object(forKey: "title") as! String
@@ -53,7 +78,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         URLSession.shared.dataTask(with: NSURL(string: url)! as URL, completionHandler: { (data, response, error) -> Void in
             
             if error != nil {
-                //print(error)
                 return
             }
             DispatchQueue.main.async(execute: { () -> Void in
--- flicks/TheMovieDBHelper.swift
+++ flicks/TheMovieDBHelper.swift
@@ -1,17 +1,9 @@
-//
-//  TheMovieDBHelper.swift
-//  flicks
-//
-//  Created by Kristy Caster on 11/4/16.
-//  Copyright © 2016 kristeaac. All rights reserved.
-//
-
 import Foundation
 
 class TheMovieDBHelper {
     
-    static func getNowPlaying(callback: @escaping ([NSDictionary]?, Int, Int, Int) -> Void) {
-        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")
+    static func getNowPlaying(page: Int, callback: @escaping ([NSDictionary]?, Int, Int, Int) -> Void) {
+        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&page=\(page)")
         let request = URLRequest(url: url!)
         let session = URLSession(
             configuration: URLSessionConfiguration.default,
